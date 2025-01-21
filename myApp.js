af0429fbb034ead28c96db34d6fa796d39bcd9b5
Edson J Vargas <edson.jhair@yahoo.com>
2025-01-21 00:40:00 +0000

    Trying to solve

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World")
+
 app.get("/", (req, res)=>{
   res.send("Hello Express")
 })
@@ -9,7 +11,6 @@ app.get("/", (req, res)=>{
 
 
 
-console.log("Hello World")
 
 
 
