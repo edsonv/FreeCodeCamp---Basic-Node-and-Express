7b6f69fa392309136eed1b78cd6bde37c5e64212
Edson J Vargas <edson.jhair@yahoo.com>
2025-01-21 03:17:09 +0000

    Implement a Root-level request logger middleware

--- myApp.js
+++ myApp.js
@@ -7,6 +7,12 @@ require('dotenv').config();
 // app.get("/", (req, res)=>{
 //   res.send("Hello Express")
 // })
+app.use("/", (req, res, next)=>{
+  console.log(`${req.method} ${req.path} -  ${req.ip}`)
+  
+  next()
+})
+
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
