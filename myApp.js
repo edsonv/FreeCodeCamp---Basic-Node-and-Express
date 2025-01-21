7a9e25ea708c9b05b1f541f4b5a5cc4953cf433a
Edson J Vargas <edson.jhair@yahoo.com>
2025-01-21 01:15:49 +0000

    Send file

--- myApp.js
+++ myApp.js
@@ -1,42 +1,14 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
+// console.log("Hello World")
 
-app.get("/", (req, res)=>{
-  res.send("Hello Express")
-})
+// app.get("/", (req, res)=>{
+//   res.send("Hello Express")
+// })
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
