033247be4489bf103f7a049bbbea51dbc3b57b5a
Edson J Vargas <edson.jhair@yahoo.com>
2025-01-21 01:21:08 +0000

    Serve JSON on a specific route

--- myApp.js
+++ myApp.js
@@ -6,9 +6,14 @@ let app = express();
 // app.get("/", (req, res)=>{
 //   res.send("Hello Express")
 // })
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+app.get("/json", (req,res)=>{
+  res.json({"message":"Hello json" })
+})
 
 module.exports = app;
